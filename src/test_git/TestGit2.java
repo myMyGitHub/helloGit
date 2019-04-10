fd81f590f63ce8c0f758b6964b4ef2d124005efe
liu-jj <liu-jj@primeton.com>
2019-04-10 17:02:37 +0800

    git commit

---
+++ src/test_git/TestGit2.java
@@ -0,0 +1,11 @@
+package test_git;
+
+public class TestGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello git");		
+
+	}
+
+}
