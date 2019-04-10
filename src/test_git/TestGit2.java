3df9e52bb8ea1814343e2b81640f22b3f59389a2
liu-jj <liu-jj@primeton.com>
2019-04-10 17:24:03 +0800

    ano111

--- src/test_git/TestGit2.java
+++ src/test_git/TestGit2.java
@@ -4,7 +4,7 @@ public class TestGit2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("gggggggggggg  hello git");		
+        System.out.println("ggg---2----g  hello git");		
 
 	}
 
